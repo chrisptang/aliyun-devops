57e8a61d4afc3cb262bad26beb7478bc4c935815
Your Name <you@example.com>
2025-01-02 19:00:10 +0800

    adding new files

--- sls_client.py
+++ sls_client.py
@@ -9,6 +9,7 @@ from alibabacloud_tea_util.client import Client as UtilClient
 import os
 import pandas as pd
 from datetime import datetime, timedelta
+import time
 
 ALIBABA_CLOUD_ACCESS_KEY_ID = os.environ["ALIBABA_CLOUD_ACCESS_KEY_ID"]
 ALIBABA_CLOUD_ACCESS_KEY_SECRET = os.environ["ALIBABA_CLOUD_ACCESS_KEY_SECRET"]
@@ -86,3 +87,69 @@ def get_sls_data_by_query(
     except Exception as error:
         print(f"sql:{query}, 查询SLS失败了:{error}:{traceback.format_exc()}")
     return pd.DataFrame(sls_query_data)
+
+def get_sls_raw_data_by_query(
+    from_time: datetime,
+    to_time: datetime,
+    query: str = "",
+    project: str = "xianmu-front-end-log",
+    logstore: str = "xm-mall",
+    retry_time: int = 1,
+    line: int = 100,
+    offset: int = 0,
+) -> pd.DataFrame:
+    if retry_time < 0:
+        print(f"超过了最多重试次数")
+        return None
+    if offset % 10000 == 0:
+        print(
+            f"即将获取数据: =====>from_time:{from_time}, to_time:{to_time}, logstore:{logstore}, query:{query[0:150]}",
+        )
+
+    from_ = int(from_time.timestamp())
+    to_ = int(to_time.timestamp())
+
+    get_logs_v2headers = sls_20201230_models.GetLogsV2Headers(
+        common_headers=headers, accept_encoding="lz4"
+    )
+    get_logs_v2request = sls_20201230_models.GetLogsV2Request(
+        from_=from_,
+        to=to_,
+        query=query,
+        line=line,
+        offset=offset,
+    )
+    runtime = util_models.RuntimeOptions(
+        connect_timeout=10 * 1000,
+        read_timeout=120 * 1000,
+        no_proxy="cn-hangzhou.log.aliyuncs.com",
+        max_attempts=2,
+    )
+
+    product_view_data = []
+    try:
+        response = sls_client.get_logs_v2with_options(
+            project=project,
+            logstore=logstore,
+            request=get_logs_v2request,
+            headers=get_logs_v2headers,
+            runtime=runtime,
+        )
+        product_view_data = response.body.data
+        if offset % 10000 == 0:
+            print(f">=====数据条数:{len(product_view_data)}")
+        return pd.DataFrame(product_view_data)
+    except Exception as error:
+        print(
+            f"查询SLS失败了,重试:{retry_time},project:{project},logstore:{logstore}, 错误:{error}"
+        )
+        # 5 秒后重试一次
+        time.sleep(5)
+        return get_sls_data_by_query(
+            from_time=from_time,
+            to_time=to_time,
+            query=query,
+            project=project,
+            logstore=logstore,
+            retry_time=retry_time - 1,
+        )
